5a350268e20b58531e573b17bb2443b0e949494f
Karim <chaya.karim.omar@gmail.com>
2025-12-31 20:49:21 +0000

    N: Copy Static

--- src/main.py
+++ src/main.py
@@ -1,7 +1,35 @@
-from textnode import (TextNode, TextType)
+import os, shutil
 
 def main():
-    dummy = TextNode("Dummy text", TextType.TEXT)
-    print(dummy)
+    copy_static_to_public()
 
-main()
+def copy_static_to_public():
+    current_dir = os.path.dirname(__file__) #.../src
+    project_root = os.path.dirname(current_dir) # static_site_generator
+    public_dir = os.path.join(project_root, 'public')
+    static_dir = os.path.join(project_root, 'static')
+
+    if not os.path.exists(static_dir):
+        raise FileNotFoundError(f"Static directory not found: {static_dir}")
+
+    if os.path.exists(public_dir):
+        shutil.rmtree(public_dir)
+   
+    os.mkdir(public_dir)
+
+    check_files(static_dir, public_dir)
+
+def check_files(src, dest):
+    for path in os.listdir(src):
+        new_path = os.path.join(src, path)
+        if os.path.isdir(new_path):
+            new_dest = os.path.join(dest, path)
+            os.mkdir(new_dest)
+            print(f"{new_dest}") 
+            check_files(new_path, new_dest)
+        else:
+           shutil.copy(new_path, dest)
+           print(f"{new_path} into {dest}")
+
+if __name__ == "__main__":
+    main()
